5d2c3e7115942150fb3995f4237ab229322c3bff
Jose Daniel Rodriguez Sotela <61768197+Enderguy747@users.noreply.github.com>
2022-05-20 22:32:16 -0600

    task controller done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import cors from 'cors'
 import { taskCategoryRouter } from './routes/taskCategory.routes.js'
 import { taskRouter } from './routes/task.routes.js'
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -12,7 +14,7 @@ app.use('/api/taskCategory', taskCategoryRouter)
 app.use('/api/task', taskRouter)
 
 app.get('/', (req, res) => {
-  res.send('Home api')
+  res.status(200).send('Home api')
 })
 
 export { app }
--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,8 +1,13 @@
-// eslint-disable-next-line no-unused-vars
 import { Task } from '../models/Task.js'
-// eslint-disable-next-line no-unused-vars
 import { validateIfNotExists } from '../utils/validateIfNotExists.js'
 
+/**
+ * It takes the data from the request body, creates a new task with that data, and returns the new task
+ * as a JSON object.
+ * @param req - The request object.
+ * @param res - the response object
+ * @returns The task object is being returned.
+ */
 async function saveTask (req, res) {
   const { taskTitle, taskDescription, taskPriority, taskCompleted, taskCategoryId } = req.body
   try {
@@ -15,22 +20,35 @@ async function saveTask (req, res) {
     })
     res.status(201).json(task)
   } catch (error) {
+    console.error(error.message)
     return res.status(500).json({ message: error.message })
   }
 }
 
+/**
+ * It gets all the tasks from the database and sends them back to the client.
+ * @param req - The request object.
+ * @param res - the response object
+ * @returns an object with two properties: data and results_number.
+ */
 async function getAllTasks (req, res) {
   try {
-    const { row, count } = await Task.findAndCountAll({
+    const { count, rows } = await Task.findAndCountAll({
       attributes: ['id', 'taskTitle', 'taskDescription', 'taskPriority', 'taskCompleted', 'taskCategoryId'],
       order: [['id', 'DESC']]
     })
-    res.send({ data: row, results_number: count })
+    res.send({ data: rows, results_number: count })
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
 }
 
+/**
+ * It gets one task from the database and returns it to the user.
+ * @param req - request
+ * @param res - the response object
+ * @returns The task object.
+ */
 async function getOneTask (req, res) {
   const { id } = req.params
   try {
@@ -41,7 +59,7 @@ async function getOneTask (req, res) {
         id
       },
       attributes: ['id', 'taskTitle', 'taskDescription', 'taskPriority', 'taskCompleted', 'taskCategoryId'],
-      order: ['id', 'ASC']
+      order: [['id', 'ASC']]
 
     })
     res.status(200).json(task)
@@ -50,6 +68,12 @@ async function getOneTask (req, res) {
   }
 }
 
+/**
+ * It updates a task in the database.
+ * @param req - the request object
+ * @param res - the response object
+ * @returns The number of records that were modified.
+ */
 async function updateTask (req, res) {
   const { id } = req.params
   const { taskTitle, taskDescription, taskPriority, taskCompleted, taskCategoryId } = req.body
@@ -66,18 +90,25 @@ async function updateTask (req, res) {
       }, { where: { id } }
     )
 
-    res.status(200).json(task)
+    res.status(200).json({ message: `task updated and ${task[0]} record(s) were modified ` })
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
 }
+
+/**
+ * It deletes a task from the database if it exists.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns a promise.
+ */
 async function deleteTask (req, res) {
   const { id } = req.params
   try {
     if (await validateIfNotExists(id, Task)) return res.status(404).json({ message: 'task not found' })
 
     await Task.destroy({ where: { id } })
-    res.status(204).json({ message: 'task deleted' })
+    return res.status(200).json({ message: 'task deleted' })
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
