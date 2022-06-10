0708733cb48b3df09444537c305ce5c2379e2dee
Jose Daniel Rodriguez Sotela <61768197+Enderguy747@users.noreply.github.com>
2022-06-09 21:18:19 -0600

    variables changed

--- src/database/database.js
+++ src/database/database.js
@@ -3,10 +3,11 @@ import 'dotenv/config'
 
 /* Creating a new instance of Sequelize and passing in the database name, user, password, host, and
 dialect. */
-const sequelize = new Sequelize(process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST || 'localhost',
+const sequelize = new Sequelize(process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD, {
+    host: process.env.PGHOST || 'localhost',
+    port: process.env.PGPORT || 5432,
     dialect: 'postgres'
   })
 
